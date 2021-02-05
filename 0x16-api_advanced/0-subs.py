b7af8f181e59c0b77e162267337948616642feb9
Camilo Barreiro <1845@holbertonschool.com>
2021-02-05 02:56:24 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-""" How many subs? """
+"""
+1-top_ten
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns subscriber count of subreddit or 0 """
-    from requests import get
+    """
+        Sends a query to Reddit API
+        Returns the number of subscribers for given subreddit
+    """
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {'User-Agent': 'my-app'}
+    resp = requests.get(url, headers=headers)
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = {'user-agent': 'my-app/0.0.1'}
-
-    r = get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
-        return 0
-
-    try:
-        js = r.json()
-
-    except ValueError:
-        return 0
-
-    data = js.get("data")
-
-    if data:
-        sub_count = data.get("subscribers")
-        if sub_count:
-            return sub_count
-
-    return 0
+    return resp.json().get('data', {}).get('subscribers', 0)
