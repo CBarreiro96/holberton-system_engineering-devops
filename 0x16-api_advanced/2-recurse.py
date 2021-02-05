fbe99986f9b132f28ef4e8add61bf795507463e7
Camilo Barreiro <1845@holbertonschool.com>
2021-02-05 03:31:09 +0000

    correction requeriment

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,22 +10,17 @@ def recurse(subreddit, hot_list=[], after=""):
     hot articles for a given subreddit."""
 
     url = "https://api.reddit.com/r/{}/hot".format(subreddit)
-    headers = {'User-Agent': 'my-app/0.0.1'}
+    headers = {'User-Agent': 'my-app'}
     arg1 = {"limit": 100, "after": after}
     resp = requests.get(url, params=arg1, headers=headers)
     list_a = resp.json().get('data', {}).get('children', None)
     pagination = resp.json().get('data', {}).get('after', None)
-
     if pagination is not None:
-
         if list_a:
             for item in list_a:
                 hot_list.append(item.get("data").get("title"))
-
         if pagination is not None:
             recurse(subreddit, hot_list, pagination)
-
         return hot_list
-
     else:
         return None
